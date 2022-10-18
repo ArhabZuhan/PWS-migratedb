25152ff451fc7d91038ad3b990459ab8fda73e2d
ArhabZuhan <Zuhanarsa@gmail.com>
2022-10-18 21:03:12 +0700

    awal

---
+++ src/main/java/arhabzuhan/doaharian/DoaharianApplication.java
@@ -0,0 +1,13 @@
+package arhabzuhan.doaharian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoaharianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoaharianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arhabzuhan/doaharian/DoaharianApplicationTests.java
@@ -0,0 +1,13 @@
+package arhabzuhan.doaharian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoaharianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
